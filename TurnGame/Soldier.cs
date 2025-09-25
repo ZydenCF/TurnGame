8c82eb86090c5c3be3b7c0131c0de634b8be52c9
David Ccolcca <draigner143@gmail.com>
2025-09-25 18:59:09 -0500

    Create Soldier.cs

---
+++ TurnGame/Soldier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnGame
+{
+    class Soldier : Unit
+    {
+        internal Soldier() : base(30, 10) { }
+
+
+        public override void Attack(Base targetBase, List<IUnit> enemyUnits)
+        {
+            if (enemyUnits.Count > 0)
+            {
+                enemyUnits[0].TakeDamage(damage);
+            }
+            else
+            {
+                targetBase.TakeDamage(damage);
+            }
+        }
+    }
+}
