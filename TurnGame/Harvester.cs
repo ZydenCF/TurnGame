a5785560bd0de4bbc7d0e0f1162ccf8f21b37f66
David Ccolcca <draigner143@gmail.com>
2025-09-25 19:03:27 -0500

    Create Harvester.cs

---
+++ TurnGame/Harvester.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnGame
+{
+    class Harvester : Unit
+    {
+        internal Harvester() : base(20, 0) { }
+
+
+        public override void Attack(Base targetBase, List<IUnit> enemyUnits)
+        {
+        }
+
+        internal int Collect()
+        {
+            return 10;
+        }
+    }
+}
