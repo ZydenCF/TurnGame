e9e6d7d95c704c90162f0806350d5b16598f59a2
David Ccolcca <draigner143@gmail.com>
2025-09-25 19:09:18 -0500

    Create Base.cs

---
+++ TurnGame/Base.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnGame
+{
+    class Base
+    {
+        private int health;
+
+
+        internal Base()
+        {
+            health = 100;
+        }
+
+
+        internal void TakeDamage(int dmg)
+        {
+            health = health - dmg;
+            if (health < 0)
+            {
+                health = 0;
+            }
+        }
+
+
+        internal bool IsAlive()
+        {
+            return health > 0;
+        }
+
+
+        internal int GetHealth()
+        {
+            return health;
+        }
+    }
+}
